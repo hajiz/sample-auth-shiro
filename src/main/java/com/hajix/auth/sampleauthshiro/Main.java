d3d6b52e3a57a82cc6f3d131c4bf4c3e66591236
Amir Hossein Hajizadeh <amirhos.hajiz@gmail.com>
2018-01-01 20:29:43 -0800

    Adding basic authentication workflow
    Added the following:
    * Apache shiro lib to pom.xml
    * Hooked up slf4j for proper logging (used by shiro)
    * Basic test code

--- src/main/java/com/hajix/auth/sampleauthshiro/Main.java
+++ src/main/java/com/hajix/auth/sampleauthshiro/Main.java
@@ -1,5 +1,48 @@
 package com.hajix.auth.sampleauthshiro;
 
-public class Main {
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 
+
+public class Main {
+    public static void main(String[] args) {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:auth.ini");
+        SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+
+        tryLogin();
+        tryLogin();
+    }
+    
+    public static void tryLogin() {
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("hajix", "verysafepassword");
+            token.setRememberMe(true);
+            try {
+                currentUser.login(token);
+                System.out.println("Login successful");
+            } catch (UnknownAccountException uae) {
+                System.out.println("There is no user with username of " + token.getPrincipal());
+            } catch (IncorrectCredentialsException ice) {
+                System.out.println("Password for account " + token.getPrincipal() + " was incorrect!");
+            } catch (LockedAccountException lae) {
+                System.out.println("The account for username " + token.getPrincipal() + " is locked.");
+            } catch (AuthenticationException ae) {
+                throw ae;
+            }
+        } else {
+            System.out.println("User already authenticated");
+        }
+    }
 }
